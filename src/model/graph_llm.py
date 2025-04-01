75e076c62390ef216df9600f19bfef4fa5e9c51b
root <otaviocx@gmail.com>
2025-04-01 15:53:39 +0000

    add test

--- src/model/graph_llm.py
+++ src/model/graph_llm.py
@@ -95,7 +95,7 @@ class GraphLLM(torch.nn.Module):
     def device(self):
         return list(self.parameters())[0].device
 
-    def maybe_autocast(self, dtype=torch.bfloat16):
+    def maybe_autocast(self, dtype=torch.float32):
         # if on cpu, don't use autocast
         # if on gpu, use autocast with dtype if provided, otherwise use torch.float16
         enable_autocast = self.device != torch.device("cpu")
--- test.py
+++ test.py
@@ -20,9 +20,33 @@ from src.utils.lr_schedule import adjust_learning_rate
 args = parse_args_llama()
 seed = args.seed
 
+dataset = load_dataset[args.dataset]()
+idx_split = dataset.get_idx_split()
+
+
+args.llm_model_path = llama_model_path[args.llm_model_name]
+model = load_model[args.model_name](graph_type=dataset.graph_type, args=args, init_prompt=dataset.prompt)
+
+test_dataset = [dataset[i] for i in idx_split['test']]
+test_loader = DataLoader(test_dataset, batch_size=args.eval_batch_size, drop_last=False, pin_memory=True, shuffle=False, collate_fn=collate_fn)
+
+# Step 5. Evaluating
+os.makedirs(f'{args.output_dir}/{args.dataset}', exist_ok=True)
 path = f'{args.output_dir}/{args.dataset}/model_name_{args.model_name}_llm_model_name_{args.llm_model_name}_llm_frozen_{args.llm_frozen}_max_txt_len_{args.max_txt_len}_max_new_tokens_{args.max_new_tokens}_gnn_model_name_{args.gnn_model_name}_patience_{args.patience}_num_epochs_{args.num_epochs}_seed{seed}.csv'
 print(f'path: {path}')
 
+model = _reload_best_model(model, args)
+model.eval()
+progress_bar_test = tqdm(range(len(test_loader)))
+with open(path, "w") as f:
+    for step, batch in enumerate(test_loader):
+        with torch.no_grad():
+            output = model.inference(batch)
+            df = pd.DataFrame(output)
+            for _, row in df.iterrows():
+                f.write(json.dumps(dict(row)) + "\n")
+        progress_bar_test.update(1)
+
+# Step 6. Post-processing & compute metrics
 acc = eval_funcs[args.dataset](path)
 print(f'Test Acc {acc}')
-
